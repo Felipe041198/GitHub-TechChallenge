eb9ae09f08f69a51fc7f25dbb156e3e58b0e267e
Felipe Vieira Nunes <felipe041198@hotmail.com>
2021-06-23 10:50:11 -0300

    Initial Commit

---
+++ app/src/main/java/com/example/githubrepos/modules/SplashActivity.kt
@@ -0,0 +1,4 @@
+package com.example.githubrepos.modules
+
+class SplashActivity {
+}
---
+++ app/src/main/java/com/example/githubrepos/modules/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.githubrepos.modules.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
